f7dc28070d701d6cdd78d80964da287252aea09e
avicha <avichabc@gmail.com>
2023-01-09 23:28:08 +0800

    feat: 增加了注释

--- src/EventListener.js
+++ src/EventListener.js
@@ -1,10 +1,7 @@
-import mitt from 'mitt';
+import mitt from "mitt";
 
 export default class EventListener {
   mitt = new mitt();
-  get all() {
-    return this.mitt.all;
-  }
   $on() {
     return this.mitt.on.apply(this, arguments);
   }
--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@ import EventListener from "./EventListener";
 import { getSupportedDriverList, getStoreClass } from "./utils";
 
 class BrowserCache extends EventListener {
+  // cache options
   opts;
+  // driver is init
   __init = false;
+  // the driver string
   driver;
+  // the store object
   store;
+  // the extra key and fallback config
   keyFallbacks = [];
+  // the key pattern and fallback config
   keyRegexFallbacks = [];
+  /**
+   *
+   * @param {String} driver the driver string
+   * @param {Object} opts the store options
+   */
   constructor(driver, opts) {
     super();
     const suportedDriverList = getSupportedDriverList();
@@ -53,6 +64,9 @@ class BrowserCache extends EventListener {
       throw new Error("please input the driver as first param.");
     }
   }
+  /**
+   * init the driver
+   */
   initDriver() {
     const suportedDriverList = getSupportedDriverList();
     if (
@@ -60,61 +74,107 @@ class BrowserCache extends EventListener {
       this.opts.driver &&
       suportedDriverList.includes(this.opts.driver)
     ) {
+      // init false
       this.__init = false;
+      // get the store class
       const StoreClass = getStoreClass(this.opts.driver);
+      // the store instance object
       this.store = new StoreClass(this.opts);
+      // the store like database maybe async,so listen the ready event to be ready
       this.store.$on("ready", () => {
         this.__init = true;
         this.driver = this.opts.driver;
         this.$emit("ready");
       });
+      // if the cache expired,fire the cacheExpired event
       this.store.$on("cacheExpired", (key) => {
         this.$emit("cacheExpired", key);
       });
     }
   }
+  /**
+   *
+   * @param {String} driver the driver string
+   */
   setDriver(driver) {
     this.opts.driver = driver;
     this.initDriver();
   }
+  /**
+   * @param {String} key the cache key
+   * @returns {Boolean}  is the key exists
+   */
   existsKey() {
     return this.store.existsKey.apply(this.store, arguments);
   }
+  /**
+   *
+   * @param {String} key the cache key
+   * @param {Object} opts the options
+   * @param {Object} opts.defaultVal default value if not get it
+   * @param {Boolean} opts.withFallback if use fallback when does not get the cache value
+   * @param {Boolean} opts.refreshCache if refresh the cache result when use the fallback
+   * @returns
+   */
   async get(key, opts = {}) {
     const { defaultVal, withFallback = true, refreshCache = true } = opts;
+    // get the cache value
     const result = await this.get(key);
+    // is the result null
     const isResultInvalid =
       result === undefined || result === null || isNaN(result) || result === "";
+    // if the result is invalid and use fallback function
     if (isResultInvalid && withFallback) {
+      // get the fallback config
       const res = this.__getFallbackByKey(key);
       if (res) {
+        // get the fallback result
         const fallbackResult = await res.fallback(key);
+        // is fallback result invalid
         const isFallbackResultInvalid =
           fallbackResult === undefined ||
           fallbackResult === null ||
           isNaN(fallbackResult) ||
           fallbackResult === "";
+        // if need refresh cache, then set the fallback result as the cache value
         if (refreshCache) {
           await this.set(key, fallbackResult, { expiredTime: res.expiredTime });
         }
+        // return the default value or the fallback result
         return isFallbackResultInvalid && defaultVal !== undefined
           ? defaultVal
           : fallbackResult;
       } else {
+        // have not the fallback config then return the default value or the origin result
         return defaultVal === undefined ? result : defaultVal;
       }
     } else {
+      // if the result is valid or does not use fallback function,then return the default value or the origin result
       return isResultInvalid && defaultVal !== undefined ? defaultVal : result;
     }
   }
+  /**
+   * @param {String} key the cache key
+   * @param {Object} value the cache value
+   * @param {Object} options the cache options
+   * @returns {StoreResult}
+   */
   set() {
     return this.store.set.apply(this.store, arguments);
   }
+  /**
+   *
+   * @param {String/Regex} key the extra key or the key pattern
+   * @param {Number} expiredTime the seconds which the key expired
+   * @param {Function} fallback the fallback function when the key is not exists
+   * @returns
+   */
   fallbackKey(key, expiredTime, fallback) {
     if (!fallback && expiredTime instanceof Function) {
       fallback = expiredTime;
       expiredTime = null;
     }
+    //the extra key
     if (typeof key === "string") {
       if (
         fallback instanceof Function &&
@@ -127,6 +187,7 @@ class BrowserCache extends EventListener {
         );
       }
     }
+    // the key pattern
     if (key instanceof RegExp) {
       if (
         fallback instanceof Function &&
