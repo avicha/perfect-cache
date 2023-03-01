b9b2faf98ad34dc80fa78c470f79028bc1afa355
Lubingcheng 卢炳成 <lubc4@cn.midea.com>
2023-03-01 11:40:12 +0800

    feat: 修改了包的名字

---
+++ test/PerfectCache.test.js
@@ -0,0 +1,21 @@
+import { PerfectCache } from "../src";
+import defaultOpts from "../src/defaultOpts";
+
+describe("perfect cache should be correct", () => {
+  test("new PerfectCache()", () => {
+    const PerfectCacheInstance = new PerfectCache();
+    expect(PerfectCacheInstance.opts).toStrictEqual(defaultOpts);
+  });
+  test("new PerfectCache(driver) valid", () => {
+    const PerfectCacheInstance = new PerfectCache("memory");
+    expect(PerfectCacheInstance.opts.driver).toBe("memory");
+  });
+  test("new PerfectCache(driver) invalid", () => {
+    const initWithInvalidDriver = () => {
+      new PerfectCache("invalid");
+    };
+    expect(initWithInvalidDriver).toThrow(
+      "please input the correct driver param as the first param or in the opts params."
+    );
+  });
+});
